a6c10de94bc4bf9d2efa433b146f4924cb55d4e8
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-09-30 19:08:11 -0400

    add image upload basic styles and structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,37 @@ import React from 'react'
 import './App.css'
 import Navbar from './components/navbar'
 import Newpost from './components/newpost'
+import avatar from './assets/avatar.webp'
+import map from './assets/map.png'
+import calendar from './assets/calendar.png'
+import addImage from './assets/addimage.png'
 
 const App = () => {
 
   return (
     <div>
       <Navbar />
-      <Newpost />
+      <div className="newPostCard">
+        <div className="addPost">
+          <img src={avatar} alt="" className='avatar' />
+          <div className="postForm">
+
+            <input type="text" placeholder="What's on your mind?" className="postInput"/>
+
+            <label htmlFor="file">
+              <img src={addImage} alt="" className='fileImages' />
+              <img src={map} alt="" className='fileImages'  />
+              <img src={calendar} alt="" className='fileImages'  />
+              <button>Send</button>
+
+            </label>
+
+            <input style={{display: "none"}} type="file" id="file"/>
+
+          </div>
+        </div>
+      </div>
+      {/* <Newpost /> */}
     </div>
   )
 }
