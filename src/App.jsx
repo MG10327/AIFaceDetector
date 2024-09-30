40f11d47da5cfa972c077a60303e7af3e3cfde43
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-09-30 18:01:40 -0400

    get face detection api to return values

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
+import * as faceapi from 'face-api.js'
+import './App.css'
 
 const App = () => {
+  const imgRef = useRef()
+  const canvasRef = useRef()
+
+  const handleImage = async () => {
+    const detections = await faceapi
+    .detectAllFaces(imgRef.current, new faceapi.TinyFaceDetectorOptions())
+    .withFaceLandmarks()
+    .withFaceExpressions()
+
+    console.log(detections)
+  }
+
+
+  useEffect( () => {
+    const loadModels = () => {
+      Promise.all([
+        faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+        faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+        faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+        faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+      ])
+      .then(handleImage)
+      .catch((e) => console.log(e))
+    }
+
+  imgRef.current && loadModels()
+  }, [])
+
   return (
-    <div>App</div>
+    <div className='app'>
+      <img src="https://images.pexels.com/photos/1231230/pexels-photo-1231230.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+      alt=""
+      width="940"
+      height="650"
+      ref={imgRef}
+      crossOrigin='anonymous'/>
+
+      <canvas
+      width="940"
+      height="650"
+      ref={canvasRef}
+      ></canvas>
+
+    </div>
   )
 }
 
