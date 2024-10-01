c5cf7290b09f5e31272ccf46de2b676bf16438e5
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-09-30 22:59:44 -0400

    Add tag a friend text to ui

--- src/components/newpost.jsx
+++ src/components/newpost.jsx
@@ -5,6 +5,7 @@ import * as faceapi from 'face-api.js'
 const Newpost = ({image}) => {
     const {url, width, height} = image;
     const [faces , setFaces] = useState([])
+    const [friends, setFriends] = useState([])
 
     const imgRef = useRef()
     const canvasRef = useRef()
@@ -40,6 +41,10 @@ const Newpost = ({image}) => {
     imgRef.current && loadModels()
     }, [])
 
+    const addFriend = (e) => {
+        setFriends(prev=>({...prev, [e.target.name]: e.target.value}))
+    }
+
   return (
     <div className='container'>
 
@@ -53,16 +58,21 @@ const Newpost = ({image}) => {
             ></canvas>
             {faces.map((face, i)=>(
                 <input
+                name={`input${i}`}
                 style={{left:face[0], top: face[1] + face[3] + 5}}
                 placeholder='Tag your friend'
                 key={i}
                 className='friendInput'
+                onChange={addFriend}
                 />
             ))}
         </div>
         <div className="right-side">
             <h1>Share your post</h1>
             <input type="text" placeholder="What's on your mind?" className='rightInput' />
+            {friends && (
+                <span>with <span className='name'>{Object.values(friends) + " "}</span></span>
+            )}
             <button className='rightButton'>Send</button>
         </div>
 
